a20526ecc6309c562d8b2109685ff89ff1d6591b
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-07-17 13:43:48 -0400

    Truck module edited constructors added addCargo / unloadCargo / write / read added

--- W7/Truck.cpp
+++ W7/Truck.cpp
@@ -6,5 +6,69 @@
 using namespace std;
 namespace sdds {
 
+    Truck::Truck(const char* plateNumber, int vehicleYear, double truckCapacity, const char* truckAddress)
+        {
+        MotorVehicle::MotorVehicle(plateNumber, vehicleYear); // Call the base class constructor
+        capacity = truckCapacity;
+        currentLoad = 0.0;
+        moveTo(truckAddress);
+    }
+
+    bool Truck::addCargo(double cargo)
+    {
+        double weight = currentLoad + cargo;
+        if (weight <= capacity)
+        {
+            currentLoad += cargo;
+            return true;
+        }
+        return false;
+    }
+
+    bool Truck::unloadCargo()
+    {
+        if (currentLoad > 0) {
+            currentLoad = 0.0;
+            return true;
+        }
+        return false;
+    }
+
+    std::ostream& Truck::write(std::ostream& os)
+    {
+        MotorVehicle::write(os); // Call the base class write function
+        os << " | " << currentLoad << "/" << capacity;
+        return os;
+    }
+
+    std::istream& Truck::read(std::istream& in)
+    {
+        MotorVehicle::read(in); // Call the base class read function
+        
+        cout << "Capacity: ";
+        in >> capacity;
+
+        while (!in || capacity < 0.0) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invaild input, enter another capacity: ";
+            cin >> capacity;
+        }
+
+        cout << "Cargo: ";
+        in >> currentLoad;
+        while (!in) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invaild input, enter another cargo weight: ";
+            cin >> currentLoad;
+        }
+
+        return in;
+
+    }
+
+
+
 
 }
--- W7/Truck.h
+++ W7/Truck.h
@@ -1,9 +1,37 @@
 #ifndef SDDS_TRUCK_H_
 #define SDDS_TRUCK_H_
 #include <iostream>
+#include "MotorVehicle.h"
+
 
 namespace sdds {
+	class Truck : public MotorVehicle {
+	
+	private:
 
+		// Maximum weight of cargo the truck can carry in kilograms
+		double capacity{ 0.0 };
+
+		// Current cargo load in kilograms
+		double currentLoad{ 0.0 };
+
+	public:
+
+		Truck(){};
+
+		Truck(const char* plateNumber, int vehicleYear, double truckCapacity, const char* truckAddress);
+
+		bool addCargo(double cargo);
+
+		bool unloadCargo();
+
+		std::ostream& write(std::ostream& os);
+
+		std::istream& read(std::istream& in);
+
+
+
+	};
 
 
 }
