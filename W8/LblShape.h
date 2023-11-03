ed23a20ba63d3a4847ca6a6e615017bb8674d4f0
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-02 21:10:22 -0400

    LblShape class defined

--- W8/LblShape.h
+++ W8/LblShape.h
@@ -1,6 +1,22 @@
 #ifndef SDDS_LBLSHAPE_H_
-#define SDDS_LBLSHALE_H
+#define SDDS_LBLSHAPE_H_
 
 #include <iostream>
+#include "Shape.h"
+
+namespace sdds {
+
+	class LblShape : public Shape {
+
+		char* m_label{};
+
+	protected:
+
+		const char* lable() const;
+
+	};
+
+}
+
 
 #endif
