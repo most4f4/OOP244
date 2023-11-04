b7f47868c5ffbd5310bbafb4ee4f3d2b2cb7c8f2
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-04 15:17:54 -0400

    spacing provided, m_title globally initialized

--- W9/HtmlText.h
+++ W9/HtmlText.h
@@ -1,13 +1,17 @@
 #ifndef SDDS_HTMLTEXT_H__
 #define SDDS_HTMLTEXT_H__
+
 #include "Text.h"
+
 namespace sdds {
+
    class HtmlText :  public Text {
-      char* m_title;
+
+       char* m_title{ nullptr }; // Holds the title of the HTML conversion dynamically
+
    public:
+
       HtmlText(const char* filename=nullptr, const char* title = nullptr);
-
-
 
    };
 }
