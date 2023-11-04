1f6bfb8bed8eee4f97d4c058f35b7f7c3129fae0
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-04 13:13:57 -0400

    spacing added, commenting for resource instance variables added, iostream library added

--- W9/Text.h
+++ W9/Text.h
@@ -1,19 +1,32 @@
 #ifndef SDDS_TEXT_H__
 #define SDDS_TEXT_H__
+
+#include <iostream>
+
 namespace sdds {
+
    class Text {
-      char* m_filename ;
-      char* m_content ;
-      int getFileLength()const;
+
+       char* m_filename{ nullptr }; // Hold the name of the file dynamically
+
+       char* m_content{ nullptr }; // Holds the content of the text file dynamically. 
+
+      int getFileLength()const; // Provides read-only access to the content of the text
+
    protected:
+
       const char& operator[](int index)const;
+
    public:
+
       Text(const char* filename=nullptr);
 
 
       void read();
       virtual void write(std::ostream& os)const;
+
    };
+
 }
 #endif // !SDDS_PERSON_H__
 
