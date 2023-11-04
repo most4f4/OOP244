ef88f44b0845f26178d58d392fcc509fc7ace797
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-04 15:23:27 -0400

    two argument constructor of the derived class added

--- W9/HtmlText.cpp
+++ W9/HtmlText.cpp
@@ -1,3 +1,20 @@
 #include "HtmlText.h"
+#include "cstring.h"
+
 namespace sdds {
+
+	HtmlText::HtmlText(const char* filename, const char* title)
+
+		: Text(filename) // Pass the filename to its base class
+	{
+
+		// Check if the title is not null, then dynamically allocate the attribute
+		if (title != nullptr && title[0] != '\0') {
+
+			m_title = new char[strLen(title) + 1];
+			strCpy(m_title, title);
+
+		}
+
+	}
 }
