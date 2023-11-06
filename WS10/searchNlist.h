0059bffc508645b9a783066645396478c4c132ce
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-06 18:31:30 -0500

    Authenticity declaration added

--- WS10/searchNlist.h
+++ WS10/searchNlist.h
@@ -1,3 +1,21 @@
+/*
+*****************************************************************************
+						  Workshop - #10
+Full Name  : Mostafa Hasanalipourshahrabadi
+Student ID#: 154581227
+Email      : mhasanalipourshahrab@myseneca.ca
+Section    : NAA
+Date       : 2023-11-06
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+I have done all the coding by myself and only copied the code that my
+professor provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+
 #ifndef SDDS_SEARCHNLIST_H__
 #define SDDS_SEARCHNLIST_H__
 
@@ -65,7 +83,7 @@ namespace sdds {
 		cout << title << endl;
 
 		for (int i = 0; i < numElements; i++) {
-			cout << i + 1 << ": " << arr[i] << endl;
+			cout << i + 1 << ": " << arr[i] << endl; // REQUIREMENT 1
 		}
 
 	}
