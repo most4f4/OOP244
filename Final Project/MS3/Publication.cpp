b701dcef7eaa4b2664634444de115b9003c2258d
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-07 21:00:02 -0500

    resetDate, setRef, set, constructor and setEmpty defined

--- Final Project/MS3/Publication.cpp
+++ Final Project/MS3/Publication.cpp
@@ -4,5 +4,39 @@
 using namespace std;
 namespace sdds {
 
+	void Publication::setEmpty()
+	{
+		m_title = nullptr;
+		m_shelfId[0] = "\0";
+		m_membership = 0;
+		m_libRef = -1;
+		m_date.setToToday();
+	}
+
+	Publication::Publication()
+	{
+		setEmpty();
+	}
+
+	void Publication::set(int member_id)
+	{
+		if (member_id > 9999 && member_id <= 99999) {
+			m_membership = member_id;
+		}
+		else {
+			m_membership = 0;
+		}
+	}
+
+	void Publication::setRef(int value)
+	{
+		m_libRef = value;
+	}
+
+	void Publication::resetDate()
+	{
+		m_date.setToToday();
+	}
+
 
 }
