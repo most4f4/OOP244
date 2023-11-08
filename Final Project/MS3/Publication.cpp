3af9f17d6dab6aa42429c092fa8d89d0382d1276
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-07 20:37:44 -0500

    Class Publication is wrote in the header file

--- Final Project/MS3/Publication.cpp
+++ Final Project/MS3/Publication.cpp
@@ -5,5 +5,4 @@ using namespace std;
 namespace sdds {
 
 
-
 }
--- Final Project/MS3/Publication.h
+++ Final Project/MS3/Publication.h
@@ -2,22 +2,41 @@
 #define SDDS_PUBLICATION_H__
 
 #include <iostream>
+#include "Streamable.h"
 #include "Lib.h"
 #include "Date.h"
 
 namespace sdds {
 
-	class Publication {
+	class Publication : public Streamable {
 
 		const char* m_title{};
 		const char* m_shelfId[SDDS_SHELF_ID_LEN+1]{};
 		int m_membership{ 0 };
 		int m_libRef{ -1 };
 		Date m_date;
+		void setEmpty();
 
 	public:
 
 		Publication();
+		virtual void set(int member_id);
+		void setRef(int value);
+		void resetDate();
+		virtual char type()const;
+		bool onLoan()const;
+		Date checkoutDate()const;
+		bool operator==(const char* title)const;
+		operator const char* ()const;
+		int getRef()const;
+
+		bool conIO(ios& io)const;
+		ostream& write(ostream& os) const;
+		istream& read(istream& istr);
+		operator bool() const;
+
+		Publication(const Publication& src);
+		Publication& operator=(const Publication& src);
 
 
 	};
