79598f25f69df4231bfbf2ecef2000ff0efe098a
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-08 21:06:26 -0500

    write() function implemented

--- Final Project/MS4/Book.cpp
+++ Final Project/MS4/Book.cpp
@@ -1,5 +1,6 @@
 #include "Book.h"
 #include "Utils.h"
+#include "Lib.h"
 
 using namespace std;
 namespace sdds {
@@ -26,6 +27,49 @@ namespace sdds {
 	Book::~Book()
 	{
 		delete[] m_author;
+	}
+
+	char Book::type() const
+	{
+		return 'B';
+	}
+
+	std::ostream& Book::write(std::ostream& os) const
+	{
+		Publication::write(os);
+
+		if (conIO(os)) {
+
+			os << " ";
+
+			if (ut.strlen(m_author) > SDDS_AUTHOR_WIDTH) {
+				for (int i = 0; i < SDDS_AUTHOR_WIDTH; i++) {
+					os << m_author[i];
+				}
+			}
+			else {
+				os.setf(ios::left);
+				os.width(SDDS_AUTHOR_WIDTH);
+				os.fill(' ');
+				os << m_author;
+				os.unsetf(ios::left);
+			}
+			os << " |";
+
+		}
+		else {
+
+			os << '\t' << m_author;
+
+		}
+
+		return os;
+
+	}
+
+	std::istream& Book::read(std::istream& istr)
+	{
+		// TODO: insert return statement here
 	}
 
 
