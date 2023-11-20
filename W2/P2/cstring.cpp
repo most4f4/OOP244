71ab66b1beb500ba2cddfce1dbc989a92ba535f1
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-19 19:38:19 -0500

    Authenticity declaration added to files

--- W2/P2/cstring.cpp
+++ W2/P2/cstring.cpp
@@ -1,3 +1,22 @@
+/*
+*****************************************************************************
+						  Workshop - #2 (P2)
+Full Name  : Mostafa Hasanalipourshahrabadi
+Student ID#: 154581227
+Email      : mhasanalipourshahrab@myseneca.ca
+Section    : NAA
+Date       : 2023-05-27
+
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+I have done all the coding by myself and only copied the code that my
+professor provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
