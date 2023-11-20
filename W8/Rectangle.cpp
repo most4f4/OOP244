684862ff6cfb31fa9489cf343803f74de529f02f
most4f4 <mhasanalipourshahrab@myseneca.ca>
2023-11-20 13:41:12 -0500

    draw() implementation modified

--- W8/Rectangle.cpp
+++ W8/Rectangle.cpp
@@ -50,45 +50,45 @@ namespace sdds {
         is >> m_width;
         is.ignore(); // ignore the separator
         is >> m_height;
-
         is.ignore(1000, '\n');
 
     }
 
     void Rectangle::draw(std::ostream& os) const
     {
-        //first line
-        os << '+';
-        os.setf(ios::right);
-        os.width(m_width - 1);
-        os.fill('-');
-        os << '+' << endl;
-        os.fill(' ');
-        os.unsetf(ios::right);
-
-        //second line
-        os << '|';
-        os.setf(ios::left);
-        os.width(m_width - 2);
-        os << label() << '|' << endl;
-
-        //next (m_height - 3) lines
-        for (int i = 0; i < m_height - 3; i++) {
-            os << '|';
-            os.setf(ios::right);
+        if (m_width > 0 && m_height > 0 && LblShape::label() != nullptr) {
+            //first line
+            os << '+';
             os.width(m_width - 1);
+            os.fill('-');
+            os << '+' << endl;
             os.fill(' ');
-            os << '|' << endl;
-            os.unsetf(ios::right);
+
+            //second line
+            os << '|';
+            os.setf(ios::left);
+            os.width(m_width - 2);
+            os << label() << '|' << endl;
+            os.unsetf(ios::left);
+
+
+            //next (m_height - 3) lines
+            for (int i = 0; i < m_height - 3; i++) {
+                os << '|';
+                os.width(m_width - 1);
+                os.fill(' ');
+                os << '|' << endl;
+            }
+
+            //last line
+            os << '+';
+            os.width(m_width - 1);
+            os.fill('-');
+            os << '+' << endl;
+            os.fill(' ');
+
         }
 
-        //last line
-        os << '+';
-        os.setf(ios::right);
-        os.width(m_width - 1);
-        os.fill('-');
-        os << '+' << endl;
-        os.unsetf(ios::right);
     }
 
 }
